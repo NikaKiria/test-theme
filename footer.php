60ebbb83094e1752544f268ff0399cb70ecfc19f
Nika Kiria <nika.kiria@shopshop.ge>
2022-10-15 20:52:28 +0400

    add header and footer

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
 <?php
+/**
+ * Contains Website Footer
+ */
 
+
+ ?>
+
+
+    <?php wp_footer(); ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,2 +1,18 @@
 <?php
+/**
+ * Contains Website Header
+ */
 
+
+
+ ?>
+
+ <!doctype html>
+ <html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        
